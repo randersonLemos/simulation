5a604caef68520f161bb53b8c2868804cb0dc3c8
Randerson <randerson.lemos@gmail.com>
2020-04-21 15:01:53 -0300

    Added file otm_tensorflow.py

--- simulation/utils/data.py
+++ simulation/utils/data.py
@@ -3,22 +3,26 @@ import pandas as pd
 
 class Data:
     def __init__(self, X, y):
-        self.X = X
-        self.y = y         
-        
-    @property
+        self._X = X
+        self._y = y
+
+    def X(self):
+        return self._X
+
+    def y(self):
+        return self._y
+
     def Xy(self):
-        return pd.merge(self.X, self.y, on='ID')
-    
-    def head(self, num):
-        other = copy.deepcopy(self)
-        other.X = self.X.iloc[:num,:]
-        other.y = self.y.iloc[:num,:]  
-        return other
-     
-        
-    def tail(self, num):
-        other = copy.deepcopy(self)
-        other.X = self.X.iloc[-num:,:]
-        other.y = self.y.iloc[-num:,:]   
-        return other
+        return pd.merge(self._X, self._y, on='ID')
+
+    #def head(self, num):
+    #    other = copy.deepcopy(self)
+    #    other.X = self._X.iloc[:num,:]
+    #    other.y = self._y.iloc[:num,:]
+    #    return other
+
+    #def tail(self, num):
+    #    other = copy.deepcopy(self)
+    #    other.X = self._X.iloc[-num:,:]
+    #    other.y = self._y.iloc[-num:,:]
+    #    return other

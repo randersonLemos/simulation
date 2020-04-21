83c3d308e0064997fcfaa34954d292d5d6ecbe6d
Randerson <randerson.lemos@gmail.com>
2020-04-21 08:25:55 -0300

    Updated graphs for visualization os otm best (worst) solutions

--- simulation/setup.py
+++ simulation/setup.py
@@ -12,10 +12,6 @@ CLUSTER_NAME = 'hpc02'
 QUEUE_KIND = 'longas'
 NR_PROCESSORS = 12
 
-SIMS_FOLDER = 'SIMS'
-
-LOCAL_ROOT     = pathlib.Path('')
-REMOT_ROOT     = pathlib.PurePosixPath('')
 LOCAL_PUTT_EXE = pathlib.Path('C:/\"Program Files (x86)"/PuTTY/plink.exe')
 LOCAL_IMEX_EXE = pathlib.Path('C:/\"Program Files (x86)"/CMG/IMEX/2017.10/Win_x64/EXE/mx201710.exe')
 REMOT_IMEX_EXE = pathlib.PurePosixPath('/mnt/software/CMG/imex/2017.10/linux_x64/exe/mx201710.exe')
--- tests/otm_output_0.py
+++ tests/otm_output_0.py
@@ -9,8 +9,8 @@ from simulation.utils.otm_manager_data import OtmManagerData
 def best_solutions_distribution_values(Df, suptitle, figname):
     plt.ioff()
     fig, axs = plt.subplots(1,9, figsize=(12,4), sharey=True, gridspec_kw={'wspace':0.05})
-    fig.suptitle(suptitle, fontsize=20, y=1.01)
-    
+    fig.suptitle(suptitle, fontsize=20, y=1.0)
+
     count = 1
     groups = [tuple(Df['ZONE'].unique()[i:3+i]) for i in range(0,27,3)]
     for (ax, group) in zip(axs, groups):
@@ -18,59 +18,59 @@ def best_solutions_distribution_values(Df, suptitle, figname):
         df = Df[mask]
         df['ZONE'] = df['ZONE'].str[-2:]
         well = group[0][:-3]
-        
-        ax.scatter(x=df['ZONE'], y=df['GOR'], s=300)
-        ax.set_title(well, fontsize=14, y=0.98)
-        
+
+        ax.scatter(x=df['ZONE'], y=df['GOR'], marker='s', s=125)
+        ax.set_title(well, fontsize=14, pad=2.5)
+
         ax.margins(0.25, 0.05)
-        
+
         if count == 1:
-            ax.set_ylabel('GOR', fontsize=14, labelpad=-2.5)        
-              
+            ax.set_ylabel('GOR', fontsize=14, labelpad=1.0)
+
         if count == 5:
-            ax.set_xlabel('Zones', fontsize=14, labelpad=1)
-        count += 1       
-        
+            ax.set_xlabel('Zones', fontsize=14, labelpad=1.0)
+        count += 1
+
         ax.xaxis.set_ticks_position('none')
         ax.yaxis.set_ticks_position('none')
         ax.tick_params(axis='both', which='major', pad=-2.5)
-            
+
         for idx, row in df.iterrows():
             ax.annotate(int(row['COUNT']), (row['ZONE'],row['GOR']), textcoords='offset points',  xytext=(0,0), ha='center', va='center', fontsize=12)
-        
-        
+
+
         for label in ax.get_xticklabels():
             label.set_rotation(90)
             label.set_fontsize(12)
-            
-            
+
+
         for label in ax.get_yticklabels():
             label.set_fontsize(12)
 
-            
+
     plt.savefig(figname, bbox_inches='tight')
-    
-    
-if __name__ == '__main__':   
+
+
+if __name__ == '__main__':
     omf = OtmManagerFile()
-    omf.set_project_root('H:/OTM_ICV_01S_WIDE')
+    omf.set_project_root(input('Enter with project root path:\n'))
     omf.set_simulation_folder_prefix('otm_iteration')
     omf.set_simulation_file_prefix('model')
     omf.set_result_file('otm.csv')
     omf.set_hldg_sample_file('hldg.txt')
-    
+
     omd = OtmManagerData(omf)
-    
-    data = omd.data().tail(5)    
+
+    data = omd.data().tail(5)
     data.X.columns.name = 'ZONE'
     Df = data.X.apply(pd.value_counts).T.stack().reset_index()
     Df = Df.rename(columns={'level_1':'GOR', 0:'COUNT'})
     Df['ZONE'] = Df['ZONE'].str.replace('_GOR','')
-    best_solutions_distribution_values(Df, 'Best solutions distribution and values', 'tmp/bsol.png')
-    
-    data = omd.data().head(5)    
-    data.X.columns.name = 'ZONE'   
+    best_solutions_distribution_values(Df, 'Best Solutions', 'bsol_wide.png')
+
+    data = omd.data().head(5)
+    data.X.columns.name = 'ZONE'
     Df = data.X.apply(pd.value_counts).T.stack().reset_index()
     Df = Df.rename(columns={'level_1':'GOR', 0:'COUNT'})
     Df['ZONE'] = Df['ZONE'].str.replace('_GOR','')
-    best_solutions_distribution_values(Df, 'Worst solutions distribution and values', 'tmp/wsol.png')
+    best_solutions_distribution_values(Df, 'Worst Solutions', 'wsol_wide.png')

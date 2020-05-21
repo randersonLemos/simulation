77de5fef8df84920620d8c21cac152a8c14d05c4
Randerson Lemos <randerson.lemos@gmail.com>
2020-05-21 10:43:29 -0300

    Updated templeta folder content and add binarysearch folder

--- tests/otm_report_run.py
+++ tests/otm_report_run.py
@@ -1,6 +1,6 @@
 import os
 if os.path.dirname(os.path.dirname(os.path.abspath(__file__))) not in os.sys.path: os.sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from simulation.model.rwd.builder_rwd import Builder_Rwd
+from simulation.manager.template_manager import TemplateManager
 from simulation.manager.otm_manager_file import OtmManagerFile
 from simulation.manager.run_manager import RunManager
 from simulation.run.report import Report
@@ -16,8 +16,7 @@ Report.set_exe(setup.LOCAL_REPO_EXE)
 
 
 if __name__ == '__main__':   
-    omf = OtmManagerFile()
-    omf.set_project_root('H:\OTM_ICV_01S_WIDE_RUNNING')
+    omf = OtmManagerFile(project_root='H:\OTM_ICV1_TIME1_RANGE1_3')
     omf.set_simulation_folder_prefix('otm_iteration')
     omf.set_simulation_file_prefix('model')
     
@@ -27,10 +26,10 @@ if __name__ == '__main__':
         rwd_path = irf_path.with_suffix('.rwd')
         rwo_path = irf_path.with_suffix('.rwo')
 
-        brwd = Builder_Rwd(frame_file_path='./frame/main.rwd.frame')
-        brwd.replace_mark(MARK_RWD_IRFFILE, irf_path)
-        brwd.replace_mark(MARK_RWD_RWOFILE, rwo_path)
-        brwd.write(rwd_path)
+        tlp = TemplateManager(template_file_path='./template/main.rwd.tlp')
+        tlp.replace_mark(MARK_RWD_IRFFILE, str(irf_path))
+        tlp.replace_mark(MARK_RWD_RWOFILE, str(rwo_path))
+        tlp.write(rwd_path)
     
         reports.append(Report(path_to_rwd=rwd_path, path_to_rwo=rwo_path, verbose=True, run=False))
       

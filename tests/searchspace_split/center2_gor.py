4eae4b96417d8686396b3036b50930d46ca48901
Randerson <randerson.lemos@gmail.com>
2020-06-26 15:00:45 -0300

    Split_region in GOR

---
+++ tests/searchspace_split/center2_gor.py
@@ -0,0 +1,9 @@
+import os
+import copy
+import numpy as np
+
+if os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) not in os.sys.path: os.sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from simulation.misc.simtime import Simtime
+
+print('{}'.format(300+(3700-300)*0.25))
+print('{}'.format(300+(3700-300)*0.75))
